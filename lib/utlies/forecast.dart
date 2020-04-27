3be0477874ec8c06b99db652422b2f40652a34ac
vipuluthaiah <45168922+vipuluthaiah@users.noreply.github.com>
2020-04-27 22:47:15 +0530

    Update forecast.dart

--- lib/utlies/forecast.dart
+++ lib/utlies/forecast.dart
@@ -1,7 +1,7 @@
 import 'package:intl/intl.dart';
 
 class Util{
-static String appId ="ed60fcfbd110ee65c7150605ea8aceea";
+static String appId ="Your Api ID";
 static String formatDate(DateTime dateTime){
   return new DateFormat("EEEE,MMM d,y").format(dateTime);
   
